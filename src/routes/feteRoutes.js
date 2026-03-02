568a971df1fa11043e8a93cae3cbc916a5ed984f
Kaja935 <heninkajarakotoarimamy@gmail.com>
2026-03-02 20:27:32 +0300

    promo

---
+++ src/routes/feteRoutes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const Fete = require('../models/Fete');
+
+// GET toutes les fêtes
+router.get('/', async (req, res) => {
+  try {
+    const fetes = await Fete.find();
+    res.json(fetes);
+  } catch(err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// insertion
+router.post('/', async (req, res) => {
+    try {
+      const fete = new Fete(req.body);
+      await fete.save();
+      res.status(201).json(fete);
+    } catch(err) {
+      res.status(400).json({ error: err.message });
+    }
+  });
+
+module.exports = router;
